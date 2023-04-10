996a568abc1375f6167a3c4b352d846252a137af
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2023-04-10 17:30:43 -0300

    otimizacao de testes

--- testing-api.php
+++ testing-api.php
@@ -1,26 +1,83 @@
 <?php
+//registra o tempo inicial
+$initialTime = time();
 //token de autenticacao da api
 $token = 'api_key';
+//bool para ser pdf
+$is_pdf = strpos($_GET['url'], '.pdf');
 //verifica se o arquivo e pdf
-if(strpos($_GET['url'], '.pdf')){
+if($is_pdf){
     //define url da requisicao
     $url = "https://vision.googleapis.com/v1/files:annotate";
     //conversao da imagem
     $pdf_base64 = convert_img_file_in_base64($_GET['url']);
     //exibe o resultado da requisicao de conversar de pdf em texto
-    echo request_text_in_pdf($url, $token, $pdf_base64);
+    $result_translate = request_text_in_pdf($url, $token, $pdf_base64);
+    //converte a resposta em um array dividido por linhas
+    $array_result_per_lines = format_result_per_line(json_decode($result_translate), $is_pdf);
 }else{
     //define url da requisicao
     $url = "https://vision.googleapis.com/v1/images:annotate";
     //conversao da imagem
     $img_base64 = convert_img_file_in_base64($_GET['url']);
     //exibe o sultado da requisicao de conversao de imagem em texto
-    echo request_text_in_image($url, $token, $img_base64);
+    $result_translate = request_text_in_image($url, $token, $img_base64);
+    //converte a resposta em um array dividido por linhas
+    $array_result_per_lines = format_result_per_line(json_decode($result_translate), $is_pdf);
 }
 
+//armazena comparativo
+store_comparative_result($initialTime, $_GET['url'], $array_result_per_lines);
 
+echo $result_translate;
 
 /*Funcoes*/
+
+
+//desc: pega a resposta da conversao e formata em um array dividido por linhas
+//params: (obj) resposta da requisicao
+//return: (array) conversao em array da resposta
+function format_result_per_line($result_translate, $is_pdf)
+{
+    //se for um pdf
+    if($is_pdf){
+        //pega o array onde esta o texto da pagina
+        $text_page = $result_translate->responses[0]->responses[0]->fullTextAnnotation->text;
+    }else{
+        //pega o array onde esta o texto da pagina
+        $text_page = $result_translate->responses[0]->textAnnotations[0]->description;
+    }
+
+
+    //retorna resultado quebrado por linhas
+    return implode(PHP_EOL, preg_split('/\n/', $text_page));
+}
+
+//desc: gerencia o armazenamento do resultado para fins de comparacaao
+//parasm: (time) tempo inicial, (string) url, (string) resultado da conversao
+//return: nenhum
+function store_comparative_result($initialTime, $url, $result_translate){
+    //caminho para salvar resultados
+    $path = "./results";
+    //nome do arquivo
+    $file_name = "google-api-" . str_replace(".", "-", substr($url, -5)) . ".txt";
+    //se o caminho do diretorio nao existir
+    if(!is_dir($path)){
+        //cria o diretorio
+        mkdir($path, 0755);
+    }
+
+    //junta o tempo gasto com o resultado da conversao
+    $array_data = array_merge(
+        ['tempo_gasto' =>'tempo gasto: '.(time() - $initialTime).' segundos'],
+        ['url' => "url: $url"],
+        ['resultado' => "resultado: \n$result_translate"]
+    );
+
+    //salva em arquivo
+    file_put_contents("$path/$file_name", implode( PHP_EOL, ($array_data))); //armazena resultado
+
+}
 
 //desc: realiza requisicao para ober resultado em texto de pdf
 //params: (string) url da requisicao, (string) token da requisicao, (string) url do documento
